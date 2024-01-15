83d49b78323171d6210fc85f692ee4c1448fb0c9
Asura king913 <israelshedrack913@gmail.com>
2024-01-15 15:41:21 +0100

    new changes

--- git.py
+++ git.py
@@ -11,14 +11,20 @@ def run_commit():
 def run_init():
         return system("git init")
 
+def push():
+    return system("git push origin master")
+
 
 def run_git():
     files = os.listdir("/home/asuraking/Desktop/codeverse/PYTHON/Prog-example")
     if ".git" in files:
         run_add()
         run_commit()
+        push()
+    
     else:
         run_add()
         run_commit()
+        push()
 
 run_git()
