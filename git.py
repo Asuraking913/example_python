8947f33e8131828e2610d2e0870e85998bfd8985
Asura king913 <israelshedrack913@gmail.com>
2024-01-15 15:40:07 +0100

    new changes

--- git.py
+++ git.py
@@ -2,14 +2,23 @@ from os import system
 from os import listdir
 import os
 
+def run_add():
+    return system("git add .")
+
+def run_commit():
+    return system("git commit -m 'new changes'")
+
+def run_init():
+        return system("git init")
+
+
 def run_git():
     files = os.listdir("/home/asuraking/Desktop/codeverse/PYTHON/Prog-example")
     if ".git" in files:
-        return system("git add .")
-        return system("git commit -m 'new changes'")
+        run_add()
+        run_commit()
     else:
-        return system("git init")
-        return system("git add .")
-        return system("git commit -m 'new changes'")
+        run_add()
+        run_commit()
 
 run_git()
