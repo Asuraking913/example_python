035150f75a8c3b989a636dcc765cf6204beab2e3
Asuraking913 <israelshedrack913@gmail.com>
2024-01-19 11:48:41 +0100

    new changes

--- git.py
+++ git.py
@@ -1,30 +1,32 @@
 from os import system
+from os import getcwd
 from os import listdir
-import os
-
-def run_add():
-    return system("git add .")
 
 def run_commit():
     return system("git commit -m 'new changes'")
 
-def run_init():
-        return system("git init")
-
-def push():
+def run_push():
     return system("git push origin master")
 
+def run_add ():
+    return system("git add .")
 
-def run_git():
-    files = os.listdir("/home/asuraking/Desktop/codeverse/PYTHON/Prog-example")
-    if ".git" in files:
-        run_add()
-        run_commit()
-        push()
+def run_init():
+    return system("git init")
+
+def run_cmd():
+    file_path = getcwd()
+    file_list = listdir(file_path)
     
-    else:
+    if ".git" in file_list:
         run_add()
         run_commit()
-        push()
+        run_push()
+    else:
+        run_init()
+        run_add()
+        run_commit()
+        run_push()
 
-run_git()
+
+run_cmd()
