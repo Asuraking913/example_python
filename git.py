93ae99a62df6d33055c360de5b181a96eec6b00e
Asuraking913 <israelshedrack913@gmail.com>
2024-01-21 12:47:47 +0100

    new changes

--- git.py
+++ git.py
@@ -2,7 +2,7 @@ from os import system
 from os import getcwd
 from os import listdir
 
-def run_commit():
+def run_commit1():
     return system("git commit -m 'new changes'")
 
 def run_push():
@@ -18,14 +18,24 @@ def run_cmd():
     file_path = getcwd()
     file_list = listdir(file_path)
     
+    def run_commit2(message):
+        return system(f"{message}")
+    
+    def run_url(url):
+        return system(f"{url}")
+
     if ".git" in file_list:
         run_add()
-        run_commit()
+        run_commit1()
         run_push()
     else:
         run_init()
         run_add()
-        run_commit()
+        message = input("Enter your git message")
+        run_commit2()
+        url = input("Enter your remote url: ")
+        run_url(url)
+
         run_push()
 
 
